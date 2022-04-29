0dfb5082ba24e7889e8fdeb5286069e8f49ee063
David <davidmejia.05@@hotmail.com>
2022-04-28 20:10:44 -0500

    home template

--- packages/models.py
+++ packages/models.py
@@ -8,6 +8,10 @@ class Package(models.Model):
     name = models.CharField(max_length=10)
     bandwidth_quantity = models.IntegerField()
     bandwidth_unit = models.CharField(choices=bandwidth_unit_choices, max_length=4)
+    price = models.IntegerField()
 
     def __str__(self):
         return self.name
+
+    def get_bandwidth(self):
+        return '{} {}'.format(self.bandwidth_quantity, self.bandwidth_unit)
--- packages/views.py
+++ packages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Package
 
-# Create your views here.
+
+def home(request):
+    packages = Package.objects.all()
+    context = {
+        'packages': packages
+    }
+    return render(request, 'home/home.html', context=context)
+
