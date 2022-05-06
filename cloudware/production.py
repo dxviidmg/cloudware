5a62b2f8454a634b1dcd4ab3ef97c40562472178
David <davidmejia.05@@hotmail.com>
2022-05-06 15:20:13 -0500

    fix

--- cloudware/production.py
+++ cloudware/production.py
@@ -3,4 +3,4 @@ from .settings import *
 ALLOWED_HOSTS = ['cloudwaremx.herokuapp.com']
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.config()})
+DATABASES = {'default': dj_database_url.config()}
