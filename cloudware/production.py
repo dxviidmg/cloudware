a874096d93688928131ec87ac91ed04291ae402b
David <davidmejia.05@@hotmail.com>
2022-05-09 13:26:57 -0500

    new config db

--- cloudware/production.py
+++ cloudware/production.py
@@ -3,4 +3,7 @@ from .settings import *
 ALLOWED_HOSTS = ['cloudwaremx.herokuapp.com']
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.config()}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+print(db_from_env)
+DATABASES["default"].update(db_from_env)
