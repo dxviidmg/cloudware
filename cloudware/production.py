c99cf5901945067f5d267e37c0d159dcaee0f1f3
David <davidmejia.05@@hotmail.com>
2022-05-09 14:06:14 -0500

    otro

--- cloudware/production.py
+++ cloudware/production.py
@@ -2,8 +2,8 @@ from .settings import *
 
 ALLOWED_HOSTS = ['cloudwaremx.herokuapp.com']
 
-import dj_database_url
+#import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-print(db_from_env)
-DATABASES["default"].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#print(db_from_env)
+#DATABASES["default"].update(db_from_env)
--- cloudware/settings.py
+++ cloudware/settings.py
@@ -129,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
