4915876e756fc6432dbfccfddf1d3d528e80da54
David <davidmejia.05@hotmail.com>
2023-03-01 15:28:30 -0600

    add

--- cloudware/production.py
+++ cloudware/production.py
@@ -1,7 +1,7 @@
 from pickle import FALSE
 from .settings import *
 
-ALLOWED_HOSTS = ['cloudwaremx.herokuapp.com', 'localhost', 'www.cloudwaremx.com']
+ALLOWED_HOSTS = ['cloudwaremx.herokuapp.com', 'localhost', 'www.cloudwaremx.com', 'cloudwaremx.com']
 
 import os
 
